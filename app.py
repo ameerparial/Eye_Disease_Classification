acc6a81cf01c47832f7c5f929e8d8896e69c8087
ameerparial <111529603+ameerparial@users.noreply.github.com>
2022-12-31 12:42:23 +0500

    urllib3

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from PIL import Image
 import numpy as np
 import joblib
-import urllib.request
+import urllib3.request
 import cv2
 import pandas as pd
 from sklearn import preprocessing
